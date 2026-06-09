80c1db6cef6444e89acd0f5cb56cc76b1d1e11ab
MrLooke <lbischof27@gmail.com>
2026-06-09 16:35:11 -0400

    Fix CORS issues for local front end

--- MediaBoard.Server/Program.cs
+++ MediaBoard.Server/Program.cs
@@ -6,6 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowFrontEnd",
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:60302")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 //Repository Classes
 builder.Services.AddScoped<ISearchService, SearchService>();
@@ -14,6 +24,9 @@ builder.Services.AddScoped<ISearchService, SearchService>();
 builder.Services.AddDbContextPool<AppDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+app.UseRouting();
+app.UseCors("AllowFrontEnd");
 
 app.UseDefaultFiles();
 app.MapStaticAssets();
