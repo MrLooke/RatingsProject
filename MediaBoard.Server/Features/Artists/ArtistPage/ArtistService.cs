d0cc92b62d480e735ac668b3fcbfe8acf697dfc9
Luke Bischof <lbischof27@gmail.com>
2026-06-11 23:32:30 -0400

    Base artist get function

---
+++ MediaBoard.Server/Features/Artists/ArtistPage/ArtistService.cs
@@ -0,0 +1,25 @@
+﻿using MediaBoard.Server.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaBoard.Server.Features.Artists.ArtistPage
+{
+    public class ArtistService : IArtistService
+    {
+        private readonly AppDbContext _dbContext;
+        public ArtistService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentException(nameof(dbContext));
+        }
+
+        public async Task<Artist> GetArtistAsync(int artistId)
+        {
+            Artist? artist = await _dbContext.Artists.AsNoTracking().Where(a => a.Id == artistId).FirstOrDefaultAsync();
+            if (artist == null)
+            {
+                throw new KeyNotFoundException("Artist ID was not found.");
+            }
+
+            return artist;
+        }
+    }
+}
---
+++ MediaBoard.Server/Features/Artists/ArtistPage/IArtistService.cs
@@ -0,0 +1,9 @@
+﻿using MediaBoard.Server.Entities;
+
+namespace MediaBoard.Server.Features.Artists.ArtistPage
+{
+    public interface IArtistService
+    {
+        Task<Artist> GetArtistAsync(int artistId);
+    }
+}
