18a4a56fb03a246f85c90acb0ef5f0dcf82a2410
Luke Bischof <57969889+MrLooke@users.noreply.github.com>
2026-08-06 20:33:44 -0400

    Clean massive ef core query and fix structure issues

--- MediaBoard.Server/Features/Artists/ArtistPage/ArtistService.cs
+++ MediaBoard.Server/Features/Artists/ArtistPage/ArtistService.cs
@@ -11,44 +11,64 @@ namespace MediaBoard.Server.Features.Artists.ArtistPage
             _dbContext = dbContext ?? throw new ArgumentException(nameof(dbContext));
         }
 
-        public async Task<ArtistPageDTO> GetArtistDetailsAsync(int artistId, int? userId = null)
+        public async Task<ArtistPageDTO?> GetArtistDetailsAsync(int artistId, int? userId = null)
         {
-            var artistDetails = await _dbContext
+            var artist = await _dbContext
                 .Artists
                 .AsNoTracking()
                 .Where(a => a.Id == artistId)
-                .Select(a => new ArtistPageDTO(a.Id, a.Name, a.Description,
-                    a.Albums
-                        .OrderBy(a => a.Year == null)
-                        .ThenByDescending(al => al.Year)
-                        .Select(al => new AlbumDTO
-                        (
-                            al.Id,
-                            al.Title,
-                            al.Year,
-                            al.Format,
-                            al.Ratings.Any() ? al.Ratings.Average(r => r.Score) : null,
-                            userId.HasValue
-                                ? al.Ratings.Where(r => r.UserId == userId).Select(r => (short?)r.Score).FirstOrDefault()
-                                : null
-                        ))
-                        .ToList(),
-                    a.Albums
-                        .SelectMany(al => al.Songs.Select(s => new TopSongDTO(
-                            s.Id,
-                            s.Title,
-                            al.Id,
-                            al.Title,
-                            s.SongRatings.Any() ? s.SongRatings.Average(r => (double?)r.Score) : null
-                        )))
-                        .OrderByDescending(s => s.AverageRating.HasValue)
-                        .ThenByDescending(s => s.AverageRating)
-                        .Take(10)
-                        .ToList()
-                ))
+                .Select(a => new { a.Id, a.Name, a.Description })
                 .FirstOrDefaultAsync();
 
-            return artistDetails;
+            if (artist is null) return null;
+
+            var albums = await GetAlbumsForArtistAsync(artistId, userId);
+            var topSongs = await GetTopSongsForArtistAsync(artistId, userId);
+
+            return new ArtistPageDTO(artist.Id, artist.Name, artist.Description, albums, topSongs);
+        }
+
+        public async Task<List<AlbumDTO>> GetAlbumsForArtistAsync(int artistId, int? userId)
+        {
+            var albums = await _dbContext
+                .Albums
+                .AsNoTracking()
+                .Where(a => a.Artists.Any(art => art.Id == artistId))
+                .OrderBy(a => a.Year == null)
+                .ThenByDescending(a => a.Year)
+                .Select(a => new AlbumDTO(
+                    a.Id,
+                    a.Title,
+                    a.Year,
+                    a.Format,
+                    a.Ratings.Count > 0 ? a.Ratings.Average(r => r.Score) : null,
+                    userId.HasValue
+                        ? a.Ratings.Where(r => r.UserId == userId).Select(r => (short?)r.Score).FirstOrDefault()
+                        : null
+                ))
+                .ToListAsync();
+
+            return albums;
+        }
+
+        public async Task<List<TopSongDTO>> GetTopSongsForArtistAsync(int artistId, int? userId)
+        {
+            var songs = await _dbContext
+                .Songs
+                .AsNoTracking()
+                .Where(s => s.Album.Artists.Any(art => art.Id == artistId))
+                .OrderByDescending(s => s.SongRatings.Average(r => r.Score))
+                .Take(10)
+                .Select(s => new TopSongDTO(
+                    s.Id,
+                    s.Title,
+                    s.Album.Id,
+                    s.Album.Title,
+                    s.SongRatings.Average(r => r.Score)
+                ))
+                .ToListAsync();
+
+            return songs;
         }
     }
 }
